2578ef7de0b206df1c70598f9cf029031511e4d8
David Sveningsson <ext@sidvind.com>
2014-01-30 22:18:08 +0100

    handle multiple files

--- jpegsplit.c
+++ jpegsplit.c
@@ -26,7 +26,7 @@
 #include <endian.h>
 #include <getopt.h>
 
-#define VERSION "1.2"
+#define VERSION "1.3"
 
 enum {
 	SOI = 0xFFD8,
@@ -123,6 +123,25 @@ static struct format* match_signature(const unsigned char* ptr){
 	return NULL;
 }
 
+static const char* name(const struct format* format){
+	return format ? format->name : "unknown data";
+}
+
+static void output(const char* filename, const struct format* format, const unsigned char* begin, const unsigned char* end, const unsigned char* ref){
+	const size_t bytes = end - begin;
+
+	fprintf(stderr, "jpegsplit: %s found at offset 0x%zx (%zd bytes)", name(format), begin-ref, bytes);
+	if ( !filename ){
+		fputc('\n', stderr);
+		return;
+	}
+
+	fprintf(stderr, ", writing to %s\n", filename);
+	FILE* dst = fopen(filename, "w");
+	fwrite(begin, bytes, 1, dst);
+	fclose(dst);
+}
+
 static const char* shortopts = "hv";
 static const struct option longopts[] = {
 	{"help",      no_argument,       0, 'h'},
@@ -132,11 +151,10 @@ static const struct option longopts[] = {
 
 static void show_usage(){
 	printf(
-		"Detect and extract additional data efter end of JPEG-images.\n"
-		"If no destination is set it returns 0 if extra data was found or 1 if pure jpeg.\n"
-		"If FIRST is set it will contain the first image without the extra data.\n"
+		"Detect and extract additional data efter end of images.\n"
+		"If no destination is set it returns 0 if extra data was found or 1 if the file was pure.\n"
 		"\n"
-		"usage: jpegsplit SRC [[FIRST] DST]\n"
+		"usage: jpegsplit SRC [DST..]\n"
 		"  -v, --version      Show version and exit.\n"
 		"  -h, --help         This text.\n"
 	);
@@ -170,9 +188,8 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 
-
 	/* open src */
-	const char* src_filename = argv[optind];
+	const char* src_filename = argv[optind++];
 	int src = open(src_filename, O_RDONLY);
 	if (src == -1){
 		error("open");
@@ -195,6 +212,7 @@ int main(int argc, char* argv[]){
 
 	/* setup pointers */
 	const unsigned char* ptr = data;
+	const unsigned char* begin = NULL;
 	const unsigned char* end = data + sb.st_size;
 
 	/* try known formats */
@@ -204,32 +222,39 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 
-	/* detect presence of additional data. */
-	ptr = format->func(ptr + format->signature_size, end);
-	const int extra_bytes = end - ptr;
-	if ( extra_bytes == 0){
+	/* loop over the files until eof or unknown data */
+	int num_found = 0;
+	for ( ;; num_found++ ){
+		begin = ptr;
+
+		/* check if this is a known format */
+		if ( !(format && format->func) ){
+			break;
+		}
+
+		/* parse file and detect presence of additional data. */
+		ptr = format->func(ptr + format->signature_size, end);
+		const size_t bytes_left = end - ptr;
+		if ( bytes_left == 0 ){
+			break;
+		}
+
+		/* write current file (detected extra file is written in next pass) */
+		const char* filename = (optind<=argc) ? argv[optind++] : NULL;
+		output(filename, format, begin, ptr, data);
+
+		/* guess next format */
+		format = match_signature(ptr);
+	}
+
+	if ( num_found == 0 ){
 		fprintf(stderr, "jpegsplit: no additional data found\n");
 		return 1;
 	}
 
-	/* no output filename given, just return successful */
-	if ( num_files <= 1 ){
-		struct format* format = match_signature(ptr);
-		fprintf(stderr, "jpegsplit: %s found at offset 0x%zx, pass an extra filename to save it\n", format ? format->name : "data", ptr-data);
-		return 0;
-	}
-
-	/* write extra data to new file */
-	FILE* dst = fopen(argv[argc-1], "w");
-	fwrite(ptr, extra_bytes, 1, dst);
-	fclose(dst);
-
-	/* write first data to new file */
-	if ( num_files >= 3 ){
-		dst = fopen(argv[argc-2], "w");
-		fwrite(data, ptr-data, 1, dst);
-		fclose(dst);
-	}
+	/* write last file */
+	const char* filename = (optind<=argc) ? argv[optind++] : NULL;
+	output(filename, format, begin, end, data);
 
 	return 0;
 }
